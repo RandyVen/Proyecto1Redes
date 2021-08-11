e225924e52b24bcd5e325dd03c6176deeb0565ea
RandyVen <35413122+RandyVen@users.noreply.github.com>
2021-08-11 03:24:54 -0600

    Update falta

--- clientfinal.py
+++ clientfinal.py
@@ -1,6 +1,14 @@
 from sleekxmpp import ClientXMPP
 from sleekxmpp.xmlstream.stanzabase import ET, ElementBase
+from sleekxmpp.plugins.xep_0096 import stanza, File
+from sleekxmpp.exceptions import IqError, IqTimeout
+#slixmpp
+import logging
 import threading
+from prettytable import PrettyTable
+import base64
+from datetime import datetime
+import time
 
 
 class Register(ClientXMPP):
@@ -10,14 +18,41 @@ class Register(ClientXMPP):
         self.add_event_handler("session_start", self.start)
         self.add_event_handler("register", self.registerAccount)
 
-        self.register_plugin('xep_0030')
-        self.register_plugin('xep_0199') 
-        self.register_plugin('xep_0004') 
-        self.register_plugin('xep_0077') 
-        self.register_plugin('xep_0045') 
-        self.register_plugin('xep_0096') 
+        self.register_plugin('xep_0030') # Service Discovery
+        self.register_plugin('xep_0199') # XMPP Ping
+        self.register_plugin('xep_0004') # Data forms
+        self.register_plugin('xep_0077') # In-band Registration
+        self.register_plugin('xep_0045') # Mulit-User Chat (MUC)
+        self.register_plugin('xep_0096') # File transfer
 
+    #Metodo inicial
+    def start(self, event):
+        self.send_presence()
+        self.get_roster()
+        self.disconnect()
 
+    #Metodo el cual registra al usuario por medio de stanza Iq
+    def registerAccount(self, iq):
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
+
+        try:
+            resp.send(now=True)
+            print("Usuario creado  - %s!" % self.boundjid)
+            log = logging.getLogger("my-logger")
+            log.info("Usuario creado  - %s!" % self.boundjid)
+        except IqError as e:
+            print("No se pudo realizar el registro: %s" % e.iq['error']['text'])
+            log.error("No se pudo realizar el registro: %s" % e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            print("No hay respuesta del servidor.")
+            log.error("No hay respuesta del servidor.")
+            self.disconnect()
+
+# Clase de cliente la cual tiene todos los metodos cuando el usuario esta logueado
 class Client(ClientXMPP):
     def __init__(self, jid, password):
         ClientXMPP.__init__(self, jid, password)
@@ -31,3 +66,75 @@ class Client(ClientXMPP):
         # Para concurrencia al momento de consultar al servidor todos los usuarios
         self.received = set()
         self.presences_received = threading.Event()
+
+
+        self.register_plugin('xep_0030') # Service Discovery
+        self.register_plugin('xep_0004') # Data forms
+        self.register_plugin('xep_0066') # Out-of-band Data
+        self.register_plugin('xep_0077') # In-band Registration
+        self.register_plugin('xep_0199') # XMPP Ping
+        self.register_plugin('xep_0045') # Multi-User Chat (MUC)
+        self.register_plugin('xep_0096') # File transfer
+
+        self.nick = ""
+
+    # Metodo de inicio para envio de presencia de conexion y obtener el roster
+    def start(self, event):
+        try:
+            log = logging.getLogger("my-logger")
+            self.send_presence(pshow='chat',pstatus='How you doin')
+            self.get_roster()
+        except IqError as e:
+            print("Could not login: %s" % e.iq['error']['text'])
+            log.error("Could not login: %s" % e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            print("No response from server.")
+            log.error("No response from server.")
+            self.disconnect()
+    
+    # Metodo para cerrar sesion
+    def logout(self):
+        print("Desconectado")
+        self.disconnect(wait=True)
+
+    # Metodo para event handler en tal caso que mi nombre de usuario fue escrito en algun room
+    def group_mention(self, msg):
+        if msg['mucnick'] != self.nick and self.nick in msg['body']:
+            user = str(msg['from'])
+            index = user.find('@conference')
+            print('\n')
+            print("<< NOTIFICATION >>")
+            print(user[:index]+': han mencionado tu nickname en el grupo y fue => '+msg['mucnick']+'\n')
+            print('Ve y revisa que han dicho de ti \n')
+            print("<< >>")
+            print('\n')
+
+    # Metodo para event handler que avisa si algun usuario se ha suscrito a mi
+    def new_user_suscribed(self,presence):
+        print('\n')
+        print("<< NOTIFICATION >>")
+        print(str(presence['from'])+' te ha agregado! ')
+        print("<< >>")
+        print('\n')
+
+    # Metodo para event handler que recibe los mensajes, y muestra notificacion dependiendo del tipo
+    # Puede ser chat o groupchat o un archivo
+    def message(self,msg):
+        if str(msg['type']) == 'chat':
+            if str(msg['subject']) == 'send_file' or len(msg['body']) > 500: #If para validar si esta entrando un archivo
+                print("** Te han enviado un archivo **")
+                img_body = msg['body']
+                file_ = img_body.encode('utf-8')
+                file_ = base64.decodebytes(file_)
+                with open("xmpp_"+str(int(time.time()))+".png","wb") as f:
+                    f.write(file_)
+            else: #Sino es un mensaje normal
+                print("** Mensaje de "+str(msg['from'])+" **")
+                print(str(msg['body']))
+                print("** **")
+        elif str(msg['type']) == 'groupchat': #En caso que sea un mensaje grupal
+            print("** mensaje grupal **")
+            print('\n  (%(from)s): %(body)s' %(msg))
+            print("** **")
+    
--- menufinal.py
+++ menufinal.py
@@ -1,12 +1,12 @@
 from client import Register,Client
-
+import os
 
 DOMAIN = '@alumchat.xyz'
 
 
 if __name__ == '__main__':
-    username = 'rands'
-    password = '123'
+    username = 'marciano44'
+    password = 'marciano441'
 
     active = True
     login_flag = False
@@ -29,7 +29,111 @@ if __name__ == '__main__':
     8. Unirse a grupo \n
     9. Enviar mensaje a grupo \n
     10. Crear un grupo \n
-    11. Definir mensaje de presencia \n
     12. Enviar archivo \n
     ===============================================
     """
+    # Ciclo que se ejecuta hasta que el usuario decide cerrar el programa
+    while active:
+        # Para mostrar el menu dependiendo si esta loqgueado o no el usuario
+        if login_flag:
+            print(menu_login)
+        else:
+            print(menu_logout)
+        opcion = input('Ingrese una opcion')
+
+        if opcion == '1': #Registrar un usuario
+            username = input('Ingrese nombre usuario: ')
+            password = input('Ingrese contraseña: ')
+            register = Register(username+DOMAIN, password)
+            if register.connect():
+                register.process(block=True)
+            else:
+                print("No ha sido posible conectarse")
+
+        elif opcion == '2' and login_flag == False: #Iniciar sesión
+            username = input('Ingrese nombre usuario: ')
+            password = input('Ingrese contraseña: ')
+            cliente = Client(username+DOMAIN, password)
+            if cliente.connect():
+                cliente.process()
+                print("Conectado")
+                login_flag = True
+            else:
+                print("No fue posible conectarse")
+
+        elif opcion == '2' and login_flag == True: #Logout
+            if cliente.connect():
+                cliente.logout()
+                login_flag = False
+            else:
+                print("No fue posible desconectarse")
+
+        elif opcion == '3' and login_flag == True: #Eliminar cuenta
+            seguro = input("¿Esta totalmente seguro de eliminar la cuenta? (y/n)  ")
+            if seguro == 'y':
+                cliente.unregister()
+                login_flag = False
+
+        elif opcion == '4' and login_flag == True: #Mostrar usuarios
+            cliente.getUsers()
+        
+        elif opcion == '5' and login_flag == True: #Agregar usuarios
+            user = input("Ingrese el usuario que desea agregar: ")
+            cliente.saveUser(user+DOMAIN)
+        
+        elif opcion == '6' and login_flag == True: #Mostrar usuario especifico
+            user = input("Ingrese el usuario a buscar: ")
+            cliente.getUser(user)
+
+        elif opcion == '7' and login_flag == True: #Enviar mensaje
+            jid = input("Usuario a enviar el mensaje: ")
+            message = input("Mensaje a enviar")
+            cliente.sendMessage(jid,message)
+
+        elif opcion == '8' and login_flag == True: #Unirse a grupo
+            room = input("Ingrese nombre del room: \n")
+            nick = input("Ingrese apodo con el que aparecera en el room: \n")
+            cliente.joinRoom(room,nick)
+
+        elif opcion == '9' and login_flag == True: #Enviar mensaje a grupo
+            room = input("Ingrese nombre del room: ")
+            message = input("Ingrese el mensaje: ")
+            cliente.messageRoom(room,message)
+        
+        elif opcion == '10' and login_flag == True: #Unirse a grupo
+            room = input("Ingrese nombre del room: \n")
+            nick = input("Ingrese apodo con el que aparecera en el room: \n")
+            cliente.createRoom(room,nick)
+
+        elif opcion == '11' and login_flag == True: #Cambiar estado
+            print("""
+            ========== Opciones de status ==========
+            1. chat - Estas disponible para conversar \n
+            2. away - No estas disponible para IM por periodo corto de tiempo \n
+            3. xa - No estas disponible para un periodo largo de tiempo \n
+            4. dnd - Estas ocupado y no quieres interrupcciones \n
+            """)
+            state = True
+            show = 0
+            while state:
+                try:
+                    show = int(input("Ingrese una opcion de status: "))
+                    if(show>=1 and show<=4):
+                        state = False
+                except:
+                    print("Debe ingresar un valor numerico")
+            status = input("Ingrese mensaje para el status")
+            cliente.changeStatus(show,status)
+        
+        elif opcion == '12' and login_flag == True: #Enviar archivo
+            path = input('Ingrese el nombre del archivo: ')
+            path_ = os.path.join(os.path.expanduser('~'),'Desktop','Images',path)
+            dest = input('Ingrese al destinatario: ')
+            cliente.sendFile(path_,dest)
+
+
+        elif (opcion == '3') and (login_flag == False): #Salir
+            active = False
+
+        else:
+            print('Ha ingresado una opcion que no es valida, intente de nuevo')
