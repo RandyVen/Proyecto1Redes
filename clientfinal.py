6979172804e536263f1cdd50f1994bcbdd2c17f5
RandyVen <35413122+RandyVen@users.noreply.github.com>
2021-08-11 18:21:42 -0600

    Update 2.2

--- clientfinal.py
+++ clientfinal.py
@@ -2,11 +2,11 @@ from sleekxmpp import ClientXMPP
 from sleekxmpp.xmlstream.stanzabase import ET, ElementBase
 from sleekxmpp.plugins.xep_0096 import stanza, File
 from sleekxmpp.exceptions import IqError, IqTimeout
-#slixmpp
+#slixmpp no me conecta por alguna razon :(
 import logging
-import threading
+import threading #para concurrencia
 from prettytable import PrettyTable
-import base64
+import base64 # para envio de archivos
 from datetime import datetime
 import time
 
@@ -17,7 +17,6 @@ class Register(ClientXMPP):
         ClientXMPP.__init__(self, jid, password)
         self.add_event_handler("session_start", self.start)
         self.add_event_handler("register", self.registerAccount)
-
         self.register_plugin('xep_0030') # Service Discovery
         self.register_plugin('xep_0199') # XMPP Ping
         self.register_plugin('xep_0004') # Data forms
@@ -82,15 +81,15 @@ class Client(ClientXMPP):
     def start(self, event):
         try:
             log = logging.getLogger("my-logger")
-            self.send_presence(pshow='chat',pstatus='How you doin')
+            self.send_presence(pshow='chat',pstatus='como estas')
             self.get_roster()
         except IqError as e:
-            print("Could not login: %s" % e.iq['error']['text'])
-            log.error("Could not login: %s" % e.iq['error']['text'])
+            print("No se pudo loguear: %s" % e.iq['error']['text'])
+            log.error("No se pudo logear: %s" % e.iq['error']['text'])
             self.disconnect()
         except IqTimeout:
-            print("No response from server.")
-            log.error("No response from server.")
+            print("No hay respuesta del server.")
+            log.error("No hay respuesta del server .")
             self.disconnect()
     
     # Metodo para cerrar sesion
@@ -137,4 +136,54 @@ class Client(ClientXMPP):
             print("** mensaje grupal **")
             print('\n  (%(from)s): %(body)s' %(msg))
             print("** **")
-    
+    # Para enviar un mensaje grupal a un room especifico
+    def messageRoom(self,room,msg):
+        try:
+            self.send_message(mto=room+'@conference.alumchat.xyz',mbody=msg, mtype='groupchat')
+            print("Mensaje enviado a: "+room)
+        except IqError:
+            print("No response from server.")
+
+    # Para unirse a un room
+    def joinRoom(self, room, nick):
+        status = "Hello :D"
+        print("Te vas a unir al room ")
+        self.plugin['xep_0045'].joinMUC(room+'@conference.alumchat.xyz', nick, pstatus=status, pfrom=self.boundjid.full, wait=True)
+        self.nick = nick
+
+    # Para crear y unirse a un room
+    # Diferencia con joinRoom es la afiliacion para ser administrador y desbloquear el room para que cualquier usuario pueda ingresar
+    def createRoom(self, room, nick):
+        status = "Hello"
+        print("Te vas a unir al room ")
+        self.plugin['xep_0045'].joinMUC(room+'@conference.alumchat.xyz', nick, pstatus=status, pfrom=self.boundjid.full, wait=True)
+        self.nick = nick
+        # Para afiliacion de adminstrador y desbloquear el grupo
+        self.plugin['xep_0045'].setAffiliation(room+'@conference.alumchat.xyz', self.boundjid.full, affiliation='owner')
+        self.plugin['xep_0045'].configureRoom(room+'@conference.alumchat.xyz',ifrom=self.boundjid.full)
+
+    # Cambiar el status e ingresar mensaje
+    def changeStatus(self, show, status):
+        show_text = ""
+        if(show == 1):
+            show_text = "chat"
+        elif(show == 2):
+            show_text = "away"
+        elif(show == 3):
+            show_text = "xa"
+        elif(show == 4):
+            show_text = "dnd"
+
+        self.send_presence(pshow=show_text, pstatus=status)
+
+    # Para suscribirte a un usuario
+    def saveUser(self,jid):
+        self.send_presence_subscription(pto=jid)
+
+    # Para enviar un mensaje de tipo chat a un usuario en especifico
+    def sendMessage(self,jid,message):
+        try:
+            self.send_message(mto=jid+'@alumchat.xyz',mbody=message, mfrom=self.boundjid.user, mtype='chat')
+            print("Mensaje enviado a: "+jid)
+        except IqError:
+            print("No response from server.")
